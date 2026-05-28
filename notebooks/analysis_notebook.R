55dd20959ddaa109ce025c64cc3e6c2869148bad
Chris Kucewicz <cfkucewicz@gmail.com>
2026-05-28 13:25:08 -0500

    Updating analysis notebook and html file

--- notebooks/analysis_notebook.R
+++ notebooks/analysis_notebook.R
@@ -187,7 +187,7 @@ pc_interactive <- fig %>%
       text    = "<b>Per Capita City Spending by Department, 2001–2024</b><br><i style='font-size:13px;color:#5a6a7a'>Inflation-adjusted to 2024 USD · Click a department in the legend to show or hide it</i>",
       font    = list(family = "Arial", size = 20, color = "#000000"),
       x       = 0.03,
-      y       = 0.875,
+      y       = 0.83,
       xanchor = "left",
       pad     = list(l = 5)
     ),
